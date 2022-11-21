0781ba8aae033946c4b5b4ad434f85cfa73440a3
Brian Prost <brian.s.prost@gmail.com>
2022-11-21 13:29:30 -0500

    renamed project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Mutable
+# _moreLikeNoDash
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mutable'`, () => {
+  it(`should have as title '_moreLikeNoDash'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mutable');
+    expect(app.title).toEqual('_moreLikeNoDash');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mutable app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('_moreLikeNoDash app is running!');
   });
 });
